e37994347f63270318d4a19d6e6d51de1ddcf920
Bradford Tuckfield <43516676+bradfordtuckfield@users.noreply.github.com>
2019-01-03 18:57:16 -0600

    updated Lesson 5

--- Lesson05/Lesson5.R
+++ Lesson05/Lesson5.R
@@ -1,43 +1,78 @@
-#### libs
-library(dplyr)
-library(rstudioapi)
-library(imager)
+#this is a file containing code for Applied Unsupervised Learning with R
+#this file contains code for Lesson 5
 
-#### there i try read arguments from comand line
-args = commandArgs(trailingOnly=TRUE)
 
-#### there i found script folder paht to set it as work directory
-current_path <- rstudioapi::getActiveDocumentContext()$path
-setwd(dirname(current_path))
+#we will start with hashing methods
 
-# test if there is at least one argument: if not, return an error
-if (length(args)==0) {
-  stop("At least one argument must be supplied (input file).\n", call.=FALSE)
+#specify a string that we want to hash
+string_to_hash<-"print('I love you')"
+
+#total number of possible hashes
+total_possible_hashes<-10000
+
+#string-to-numeric conversion
+numeric<-utf8ToInt(string_to_hash)
+print(numeric)
+
+#calculate the hash
+hash<-sum((numeric+123)^2) %% total_possible_hashes
+
+#compile all of the code into a function
+get_hash<-function(string_to_hash, total_possible_hashes){
+numeric<-utf8ToInt(string_to_hash)
+hash<-sum((numeric+123)^2) %% total_possible_hashes
+return(hash)
 }
 
-#### create empty final matrix
+#create different inputs so we can compare hashes
+script_1<-"print('I love you')"
+script_2<-"print('I lose you')"
+script_3<-"print('I hate you')"
+script_4<-"print('I love lamp')"
+
+#compare hashes of different inputs
+print(get_hash(script_1,10000))
+print(get_hash(script_2,10000))
+print(get_hash(script_3,10000))
+print(get_hash(script_4,10000))
+
+#calculate an MD5 hash of our script
+install.packages('digest')
+library(digest)
+print(digest(string_to_hash,algo='md5'))
+
+
+#section on analytic signatures
+
+#read in the image whose signature you want
+#note: make sure that your images are saved to R's working directory
+
+
+#in this case, we will use the philly city hall image
+filepath<-'philly_city_hall.jpg'
+#note: if you set filepath<-'borges.jpg', you can get a signature of the borges image
+#note: if you set filepath<-'philly_city_hall_altered.jpg', you can get a signature of the altered City Hall image
+
+im <- imager::load.image(file =filepath) 
+
+#convert to grayscale and split into sections
+im<-imager::rm.alpha(im)
+im<-imager::grayscale(im)
+im<-imager::imsplit(im,axis = "x", nb = 10)   
+
+#create a matrix of brightness values
 matrix <- matrix(nrow = 10, ncol = 10)
 
-#### load - convert to grayscale - divide image to 10 columns
-im <- imager::load.image(file ='cityhall_watermark.jpg') %>% #imager::load.image(file = args[1]) %>% 
-  imager::rm.alpha() %>% 
-  imager::grayscale() %>% 
-  imager::imsplit(axis = "x", nb = 10) 
-
-#### divide every column to 10 rows - calculate average brightness - write ti matrix 
+#fill the matrix with brightness values
 for (i in 1:10) {
   is <- imager::imsplit(im = im[[i]], axis = "y", nb = 10)
   for (j in 1:10) {
     matrix[j,i] <- mean(is[[j]])
-    #save.image(im = is[[j]], file = paste0("/home/bradfordtuckfield/Documents/packt/image_", j, "_",i, ".jpg"))
   }
 }
-
-#### print matrix
 print(matrix)
 
-#check whether brighter or less bright than neighbors.
-
+#create a function for brightness comparison
 brightnesscomparison<-function(x,y){
 compared<-0
 if(abs(x/y-1)>0.1){
@@ -55,6 +90,17 @@ compared<-(-1)
 return(compared)
 }
 
+#compare a section's brightness with the section on its left
+i<-5
+j<-5
+left<-brightnesscomparison(matrix[i,j-1],matrix[i,j])
+
+#compare a section with the section above it
+i<-5
+j<-5
+top<-brightnesscomparison(matrix[i-1,j],matrix[i,j])
+
+#create a function that compares a section with all of its neighbors
 neighborcomparison<-function(mat,i,j){
 comparison<-NULL
 
@@ -73,8 +119,6 @@ if(j>1 & i<nrow(mat)){
 bottom_left<-brightnesscomparison(mat[i+1,j-1],mat[i,j])
 }
 
-
-
 top_right<-0
 if(i>1 & j<nrow(mat)){
 top_right<-brightnesscomparison(mat[i-1,j+1],mat[i,j])
@@ -89,7 +133,6 @@ bottom_right<-0
 if(i<nrow(mat) & j<ncol(mat)){
 bottom_right<-brightnesscomparison(mat[i+1,j+1],mat[i,j])
 }
-
 
 top<-0
 if(i>1){
@@ -106,68 +149,68 @@ comparison<-c(top_left,left,bottom_left,bottom,bottom_right,right,top_right,top)
 return(comparison)
 }
 
-fingerprint<-NULL
+#initialize a NULL signature variable
+signature<-NULL
 
+#compile a signature by comparing each section to its neighbors
 for (i in 1:nrow(matrix)){
 
 for (j in 1:ncol(matrix)){
 
-fingerprint<-c(fingerprint,neighborcomparison(matrix,i,j))
+signature<-c(signature,neighborcomparison(matrix,i,j))
 
 }
 
 }
 
+#save this signature to a variable called "building_signature"
+building_signature<-signature
+
+#note: you can run all of the preceding code with any image file
+#if you use the borges.jpg file and save it as the "im" variable, you can get a different signature for the Borges image
+#you can save the Borges signature to a variable called "borges_signature"
+#next, we will compare the signature of the building image with the signature of the Borges image
+
+comparison<-mean(abs(borges_signature-building_signature))
+print(comparison)
+
+#we can also compare the signature of the altered building image with the signature of the original building image
+comparison<-mean(abs(watermarked_signature-building_signature))
+print(comparison)
 
 
+#the final section of this code covers factor analysis
+#first, we will install and load some useful packages:
 
-####pca on this pic
+install.packages('psych')
+install.packages('GPArotation')
+install.packages('qgraph')
 
-im <- imager::load.image(file ='/home/bradfordtuckfield/Documents/packt/building.png')
-
-
-
-im<-imager::rm.alpha(im) 
-im<-imager::grayscale(im)
-
-graypca<- prcomp(im, center = FALSE)
-
-library(jpeg)
-
-for (i in seq.int(1, round(ncol(im) - 10), length.out = round(ncol(im)/1.5))) {
-  #pca.img <- 
-  compressed.img <- t(graypca$x[,1:i] %*% t(graypca$rotation[,1:i]))
-  #sapply(graypca, function(j) {
-    #compressed.img <- graypca$x[,1:i] %*% t(graypca$rotation[,1:i])
-  #}, simplify = 'array')
-  #writeJPEG(pca.img, paste('/home/bradfordtuckfield/Documents/packt/compressed/gray_compressed_', round(i,0), '_components.jpg', sep = ''))
-  writeJPEG(compressed.img, paste('/home/bradfordtuckfield/Documents/packt/compressed/gray_compressed_', round(i,0), '_components.jpg', sep = ''))
-}
-
-
-
-
-
-###########factor analysis
-
-#factor analysis code
 library(psych)
+library(GPArotation)
 library(qgraph)
 
-#teacher<-read.csv('/home/bradfordtuckfield/Documents/packt/turkiye-student-evaluation_generic.csv',stringsAsFactors=FALSE)
-
+#load the "big5" data and check its dimensions and range
 data(big5)
 
-#teacherqs<-teacher[,6:ncol(teacher)]
+print(nrow(big5))
+print(ncol(big5))
 
-bigcor<-cor(big5)
+print(range(big5))
 
-solution <- fa(r = bigcor, nfactors = 5, rotate = "oblimin", fm = "pa") 
+#create a correlation matrix
+big_cor <- cor(big5)
 
+#perform the factor analysis
+solution <- fa(r = big_cor, nfactors = 5, rotate = "oblimin", fm = "pa") 
 
+#print the solution and the weights
+print(solution)
+print(solution$weights)
+
+#change some arguments in the factor analysis command
 solution <- fa(r = bigcor, nfactors = 3, rotate = "varimax", fm = "minres") 
 
-
+#explore how many factors we should use
 parallel <- fa.parallel(big5, fm = 'minres', fa = 'fa')
 
-print(parallel)
